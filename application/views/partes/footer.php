ad96522f0af4cf838e66e00daa5e15f36b890442
Jose Gabriel E <j-e_1997@hotmail.es>
2016-11-20 20:36:39 -0400

    Cambios en header
    Creado de validaciones en header para cuando esta logueado o no creacion
    de footer

--- application/views/partes/footer.php
+++ application/views/partes/footer.php
@@ -1,3 +1,11 @@
   </div>
+  <footer style="
+    background: #2c3e50;
+    color: #fff;
+">
+    <div class="container-fluid">
+      <h1 class="text-center">Veterinaria Jose Gabriel Escoboso</h1>
+    </dvi>
+  </footer>
 </body>
 </html>
--- application/views/secciones/registro.php
+++ application/views/secciones/registro.php
@@ -1,4 +1,5 @@
 <?php $this->load->view('partes/head',$titulo); ?>
+<section style="margin-bottom:5px">
 
 <form class="form-horizontal" action="<?php echo base_url('mascotas/guardar');?>" method="post" enctype="multipart/form-data">
   <input type="hidden" name="id" value="">
@@ -17,8 +18,16 @@
   <div class="form-group">
     <label for="" class="col-sm-2 control-label">Tipo</label>
     <div class="col-sm-10">
-    <select type="text" name="tipo" class="form-control">
+    <select type="text" name="tipo" class="form-control" required>
+      <option value="">Selcciona Tipo de Animal</option>
+      <option value="perro">Ave</option>
+      <option value="perro">Anfibio</option>
+      <option value="perro">Conejo</option>
+      <option value="perro">Gato</option>
+      <option value="perro">Pez</option>
       <option value="perro">Perro</option>
+      <option value="perro">Reptil</option>
+      <option value="perro">Roedor</option>
     </select>
     </div>
   </div>
@@ -28,10 +37,13 @@
     <input type="text" name="raza" class="form-control" value="">
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group ">
     <label for="" class="col-sm-2 control-label">Peso</label>
     <div class="col-sm-10">
-    <input type="number" step="any" name="peso" class="form-control" value="">
+      <div class="input-group">
+        <input type="number" step="any" name="peso" class="form-control" value="">
+        <span class="input-group-addon">kgr</span>
+      </div>
     </div>
   </div>
   <div class="form-group">
@@ -43,18 +55,19 @@
   <div class="form-group">
     <label for="" class="col-sm-2 control-label">Foto</label>
     <div class="col-sm-10">
-    <input type="file" name="foto" class="form-control">
+    <input type="file" name="foto" required class="form-control">
     </div>
   </div>
   <div class="form-group">
     <label for="" class="col-sm-2 control-label">Comentario</label>
     <div class="col-sm-10">
-    <textarea type="text" name="comentario" class="form-control" value=""></textarea>
+    <textarea type="text" name="comentario" class="form-control" ></textarea>
     </div>
   </div>
   <div class="col-sm-offset-2">
     &nbsp;&nbsp;<button type="submit" class="btn btn-success">Registrar</button>
   </div>
 </form>
+</section>
 
 <?php $this->load->view('partes/footer'); ?>
